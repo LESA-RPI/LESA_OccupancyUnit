e371fb372993927dcce6e02c8eda379abb65605d
Mingyi Jiang <44463735+EcneicsJiang@users.noreply.github.com>
2024-04-16 11:03:19 -0400

    Adding the ioc file for CG chip

--- Core/Src/stm32wbxx_hal_msp.c
+++ Core/Src/stm32wbxx_hal_msp.c
@@ -240,7 +240,7 @@ void HAL_UART_MspInit(UART_HandleTypeDef* huart)
     GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
     GPIO_InitStruct.Pull = GPIO_NOPULL;
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
-    GPIO_InitStruct.Alternate = GPIO_AF7_USART1;
+    GPIO_InitStruct.Alternate = GPIO_AF4_I2C1;//Modified
     HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
     /* USART1 DMA Init */
